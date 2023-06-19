d7611fba24b0ac1aaeaee6c5281d992071d2ede6
Eugene Turchenkov <x65x74@gmail.com>
2023-06-19 15:16:24 +0300

    chore(logger): add try/except block for request

--- pkg/hayloft/logger.py
+++ pkg/hayloft/logger.py
@@ -1,7 +1,10 @@
-import requests
-from nanoid import generate
+import logging
 from typing import Literal
 
+import requests
+from nanoid import generate
+
+main_logger = logging.getLogger(__name__)
 
 def logger():
     session = f"session-{generate(size=6)}"
@@ -11,9 +14,12 @@ def logger():
         message: str = "",
         type: Literal["info", "prompt", "completion", "warning", "error"] = "info",
     ) -> None:
-        requests.post(
-            "http://localhost:7000/event",
-            json={"session": session, "title": title, "message": message, "type": type},
-        )
+        try:
+            requests.post(
+                "http://localhost:7000/event",
+                json={"session": session, "title": title, "message": message, "type": type},
+            )
+        except requests.exceptions.ConnectionError:
+            main_logger.info("Hayloft didn't start")
 
     return log
