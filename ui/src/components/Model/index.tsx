4de738dab0d1508ff4a1c4796fe6249a02e76ff0
Eugene Turchenkov <x65x74@gmail.com>
2023-06-26 18:48:33 +0300

    feat(ui): add rename/remove session features

--- ui/src/components/Model/index.tsx
+++ ui/src/components/Model/index.tsx
@@ -1,13 +1,34 @@
+import { useState } from "react";
 import { useStore } from "@/store";
 import { useClickAway } from "@/hooks";
 import { format, differenceInSeconds } from "date-fns/esm/fp";
 import * as M from "@/store/mutations";
 import { service } from "@/service";
+import {
+  PencilIcon,
+  TrashIcon,
+  CheckIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/outline";
 
 export const Modal = () => {
   const { store, mutateStore } = useStore();
   const ref = useClickAway(() => mutateStore(M.closeModal));
+  const [editState, setEditState] = useState<{ id: number; name: string }>({
+    id: 0,
+    name: "",
+  });
   const now = new Date();
+
+  const updateSessionName = () =>
+    service
+      .updateSession(editState.id, {
+        name: editState.name,
+      })
+      .then((session) => {
+        mutateStore(M.updateSession(session));
+        setEditState({ id: 0, name: "" });
+      });
 
   return Boolean(store.sessionSelecting.tabId) ? (
     <>
@@ -22,23 +43,85 @@ export const Modal = () => {
             {store.sessions.map((session) => (
               <div
                 key={session.id}
-                className="px-4 py-2 select-none rounded-xl text-left flex flex-nowrap cursor-pointer hover:bg-base-200"
-                onClick={() => {
-                  mutateStore(M.selectSession(session.id));
-                  service.getEvents(session.id).then(({ events }) => {
-                    mutateStore(M.setEvents(session.id, events));
-                  });
-                }}
+                className="px-4 py-2 select-none rounded-xl text-left flex flex-nowrap hover:bg-base-200 group"
               >
-                <p className="grow">{session.name}</p>
-                <p className="flex-none">
-                  {differenceInSeconds(session.createdAt)(now) > 15
-                    ? dateFormat(session.createdAt)
-                    : "now"}
-                </p>
+                {editState.id !== session.id ? (
+                  <>
+                    <div
+                      className="grow cursor-pointer"
+                      onClick={() => {
+                        mutateStore(M.selectSession(session.id));
+                        service.getEvents(session.id).then(({ events }) => {
+                          mutateStore(M.setEvents(session.id, events));
+                        });
+                      }}
+                    >
+                      <div className="flex flex-nowrap">
+                        <p className="grow ">{session.name}</p>
+                        <p className="flex-none">
+                          {differenceInSeconds(session.createdAt)(now) > 15
+                            ? dateFormat(session.createdAt)
+                            : "now"}
+                        </p>
+                      </div>
+                    </div>
+                    <p
+                      className="flex-none ml-4 pt-px text-gray-500 cursor-pointer hover:text-gray-200 hidden group-hover:block"
+                      onClick={() =>
+                        setEditState({ id: session.id, name: session.name })
+                      }
+                    >
+                      <PencilIcon className="h-5 w-5" />
+                    </p>
+                    <p
+                      className="flex-none ml-2 pt-px text-gray-500 cursor-pointer hover:text-gray-200 hidden group-hover:block"
+                      onClick={() =>
+                        service.removeSession(session.id).then(({ id }) => {
+                          mutateStore(M.removeSession(id));
+                        })
+                      }
+                    >
+                      <TrashIcon className="h-5 w-5" />
+                    </p>
+                  </>
+                ) : (
+                  <>
+                    <div className="grow">
+                      <input
+                        type="text"
+                        autoFocus
+                        className="w-full bg-transparent text-gray-500 outline-none"
+                        value={editState.name}
+                        onChange={(e) => {
+                          setEditState({ ...editState, name: e.target.value });
+                        }}
+                        onKeyDown={(e) => {
+                          if (e.key === "Enter") {
+                            updateSessionName();
+                          }
+                        }}
+                      />
+                    </div>
+                    <p
+                      className="flex-none ml-4 pt-px text-gray-500 cursor-pointer hover:text-gray-200"
+                      onClick={updateSessionName}
+                    >
+                      <CheckIcon className="h-5 w-5" />
+                    </p>
+                    <p
+                      className="flex-none ml-2 pt-px text-gray-500 cursor-pointer hover:text-gray-200"
+                      onClick={() => setEditState({ id: 0, name: "" })}
+                    >
+                      <XMarkIcon className="h-5 w-5" />
+                    </p>
+                  </>
+                )}
               </div>
             ))}
           </div>
+          {store.sessions.length === 0 && (
+            <p className="text-gray-500 text-center">No sessions yet...</p>
+          )}
         </div>
       </div>
     </>
--- ui/src/service/index.ts
+++ ui/src/service/index.ts
@@ -3,6 +3,10 @@ import { agent } from "./agent";
 export const service = {
   getSessions: () =>
     agent("GET", "/sessions") as Promise<{ sessions: Raw.Session[] }>,
+  updateSession: (id: number, body: { name: string }) =>
+    agent("PUT", `/sessions/${id}`, body) as Promise<Raw.Session>,
+  removeSession: (id: number) =>
+    agent("DELETE", `/sessions/${id}`) as Promise<{ id: number }>,
   getEvents: (sessionId: number) =>
     agent("GET", `/sessions/${sessionId}/events`) as Promise<{
       events: Raw.Event[];
